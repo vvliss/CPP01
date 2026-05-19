0dc34b78e2bbeb67e3afa83f863fd0e6f75922e8
Wiktoria Lisson <wilisson@2-D-2.42heilbronn.de>
2026-05-19 18:47:24 +0200

    finished ex05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -1,15 +1,39 @@
 #include "Harl.hpp"
 
 Harl::Harl() {
-
 }
 
 Harl::~Harl() {
-
 }
 
-void complain(std::string level) {
-    using action = void(Harl::*)();
-    std::pair<std::string, action>;
+void Harl::debug(void) {
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+    std::cout << " " << std::endl;
+}
+
+void Harl::info(void) {
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+    std::cout << " " << std::endl;
+}
+
+void Harl::warning(void) {
+    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
+    std::cout << " " << std::endl;
+}
+
+void Harl::error(void) {
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level) {
+    typedef void (Harl::*Action)();
+    Action table[] = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    std::string list[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
     
+    for (int i = 0; i < 4; i++) {
+        if (list[i] == level) {
+            (this->*table[i])();
+            return ;
+        }
+    }
 }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -1,7 +1,20 @@
 #include "Harl.hpp"
 
-
 int main()
 {
+    Harl harl;
     
+    std::cout << "DEBUG level" << std::endl;
+    harl.complain("DEBUG");
+    
+    std::cout << "Testing INFO level" << std::endl;
+    harl.complain("INFO");
+    
+    std::cout << "Testing WARNING level" << std::endl;
+    harl.complain("WARNING");
+    
+    std::cout << "Testing ERROR level" << std::endl;
+    harl.complain("ERROR");
+    
+    return 0;
 }
